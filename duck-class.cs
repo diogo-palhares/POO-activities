8b9cf6addf89545238d7f07a636ba299398808aa
dpalhares <dpalhares@ciandt.com>
2025-04-29 08:34:02 -0300

    startando repo remoto

---
+++ duck-class.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Patos {
+    
+    public class Duck {
+
+        private string Name {get;set;}
+        private int Age {get;set;}
+    
+        public Duck(string name; int age){
+            Name = name;
+            Age = age;
+        }
+    }
+}
